68859c045f18519bd331b6ecfeef1f533a49b4e6
Ron Zacharski <raz@Ron-Zacharskis-iMac.local>
2010-02-24 17:12:49 -0700

    added real login

--- login.php
+++ login.php
@@ -9,11 +9,30 @@
 <body>
 <div id="contents">
 <?php
+  include "db_connect.php";
   $name = $_POST['username'];
+  $pw = $_POST['pw'];
 
-   echo "<p>Thanks for logging in, $name</p>\n";
+   $query = "select * from users WHERE username = '$name' AND password = '$pw'";
+   $result = mysqli_query($db, $query);
+   if ($row = mysqli_fetch_array($result))
+   {
+   		
+   		echo "<p>Thanks for logging in, $name</p>\n";
+   		echo "<p><a href=\"search.php\">Continue</a></p>";
+   }
+   else
+    {
+   		echo "<p>Incorrect username or password</p>\n";
+   		echo  "<h1>Log In</h1>\n  <form method=\"post\" action=\"login.php\">";
+    	echo "<label for=\"username\">Username:</label><input type=\"text\" id=\"username\" name=\"username\" /><br />";
+        echo "<label for=\"pw\">Password:</label><input type=\"password\" id=\"pw\" name=\"pw\" /><br />";
+        echo "<input type=\"submit\" value=\"Login\" name=\"submit\" /></form> <p><a href=\"createAccount.php\">Create Account</a></p>";
+    }
+   
+  
 ?>
-  <p><a href="search.php">Continue</a></p>
+  
   </div>
 </body>
 </html>
